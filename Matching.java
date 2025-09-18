ca4dbac938fdd9b1e967956f8f03444736370fa7
Harley Hannahs <hanna537@morris.umn.edu>
2025-09-18 14:21:44 -0500

    starting on main

--- Matching.java
+++ Matching.java
@@ -1,5 +1,22 @@
 import java.util.*;
 
 public class Matching {
-        public static 
+
+        public static int[] satisfactoryPair(int[][] progPref, int[][] comPref){
+                int n = progPref.length;
+                int[] comMatch = new int[n];
+                Arrays.fill(comMatch, -1);
+
+                boolean progFree[] = new boolean[n];
+                Arrays.fill(progFree, true);
+
+                boolean comFree[] = new boolean[n];
+                Arrays.fill(comFree,true);
+
+                int[] nextApplication = new int[n];
+
+        }
+
+        int freeCount = n;
+
 }
