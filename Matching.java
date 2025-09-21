74733862606119ee4a950ff09c73f411eea6a6a7
imbroglio-0 <hanna537@morris.umn.edu>
2025-09-21 12:05:31 -0500

    Made slight progress on satisfactoryPair method

--- Matching.java
+++ Matching.java
@@ -1,22 +1,33 @@
 import java.util.*;
 
+//References:
+//https://www.geeksforgeeks.org/dsa/stable-marriage-problem/
+
 public class Matching {
 
-        public static int[] satisfactoryPair(int[][] progPref, int[][] comPref){
-                int n = progPref.length;
-                int[] comMatch = new int[n];
-                Arrays.fill(comMatch, -1);
+        public static int[] satisfactoryPair(int n, int[][] progPref, int[][] compPref){
+
+                int[] progMatch = new int[n];
+                Arrays.fill(progMatch, -1);
+                int[] compMatch = new int[n];
+                Arrays.fill(compMatch, -1);
+                //Indicates the company/programmer match (initialized as unmatched)
 
                 boolean progFree[] = new boolean[n];
                 Arrays.fill(progFree, true);
-
-                boolean comFree[] = new boolean[n];
-                Arrays.fill(comFree,true);
+                boolean compFree[] = new boolean[n];
+                Arrays.fill(compFree,true);
+                //Indicates that the company/programmer is free
 
                 int[] nextApplication = new int[n];
+                Arrays.fill(nextApplication, 0);
+                //Keeping track of which programmer is applying to which company
 
+                return progMatch;
         }
 
-        int freeCount = n;
+        public static void main(){
+                
+        }
 
 }
